64833da9e916d2b3dcbcf0164716d2767ee428af
Dave Küppers <dave@evad.nl>
2025-02-24 23:25:39 +0100

    change histsize

--- .zsh/config/history.zsh
+++ .zsh/config/history.zsh
@@ -1,7 +1,7 @@
 # ----- History Config
-HISTSIZE=10000000                # How many lines of history to keep in memory.
+HISTSIZE=100000                  # How many lines of history to keep in memory.
 HISTFILE=$ZSH/.zsh_history       # Where to save history to disk.
-SAVEHIST=10000000                # Number of history entries to save to disk.
+SAVEHIST=100000                  # Number of history entries to save to disk.
 setopt appendhistory             # Append history file.
 setopt share_history             # Share history between multiple sessions.
 setopt hist_ignore_all_dups      # Ignore duplicates.
