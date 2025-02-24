89ca4632bced0625ee4be25473ccd19bbae60d06
Dave Küppers <evad@DaveOps.local>
2025-02-24 22:54:11 +0100

    update aliases

--- .zsh/config/aliases.zsh
+++ .zsh/config/aliases.zsh
@@ -1,5 +1,20 @@
 ## ----- aliases
 alias ls="ls --color"
+alias cat="bat"
+
+
+# kubectl
+alias k="kubectl"
+alias kgp="kubectl get pods"
+alias kgn="kubectl get nodes" 
+alias kex="kubectl exec -it"
+alias ksec="kubectl get secrets"
+alias kdp="kubectl describe"
+
+# terraform
+alias tf="terraform"
+alias tfp="terraform plan"
+alias tfa="terraform apply"
 
 # MacOS specific
 alias flushdns="sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder"
