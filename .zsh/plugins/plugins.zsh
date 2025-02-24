1ecb28eb343cc294f0f9b29321841dfc89be5102
Dave Küppers <dave@evad.nl>
2025-02-25 00:02:43 +0100

    add abrr config

--- .zsh/plugins/plugins.zsh
+++ .zsh/plugins/plugins.zsh
@@ -1,7 +1,11 @@
 ## ----- ZSH Plugins
 [[ -f $ZSH/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh ]] && source $ZSH/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh # auto completion of commands
 [[ -f $ZSH/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh ]] && source $ZSH/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh # Syntax highlighting, wrong or non existing command goes red, green is good :).
+
+## ----- zsh-abbr config
 [[ -f $ZSH/plugins/zsh-abbr/zsh-abbr.plugin.zsh ]] && source $ZSH/plugins/zsh-abbr/zsh-abbr.plugin.zsh
+export ABBR_SET_EXPANSION_CURSOR=1
+
 
 # ----- zsh-z specific settings
 [[ -f $ZSH/plugins/zsh-z/zsh-z.plugin.zsh ]] && source $ZSH/plugins/zsh-z/zsh-z.plugin.zsh
